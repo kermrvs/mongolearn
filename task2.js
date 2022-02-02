4cc0ccf0eff010943055de11de5b3a8207121f8a
Kerm <ousar99@gmail.com>
2022-02-02 16:58:42 +0200

    Huinia na aggregate

--- task2.js
+++ task2.js
@@ -5,10 +5,9 @@ db.reports.drop()
 db.createCollection('customers', {capped: true, size: 300000, max: 3000})
 db.createCollection('orders')
 db.createCollection('reports')
-
 const users = []
 const orders = [];
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 3000; i++) {
   const date = new Date();
   const user = {
     _id: i,
@@ -21,9 +20,7 @@ for (let i = 0; i < 5; i++) {
   }
   users.push(user)
 }
-
 db.customers.insertMany(users)
-
 users.forEach(value => {
   const randomOrders = Math.floor(Math.random() * 10 + 1);
   for (let i = 0; i < randomOrders; i++) {
@@ -41,22 +38,36 @@ users.forEach(value => {
 db.orders.insertMany(orders);
 
 const customers = db.customers.find();
-
 while (customers.hasNext()){
   const customer = customers.next();
-  const orders = db.orders.find({customersId:customer._id});
   const obj = {
     fName:customer.name.first,
     lName:customer.name.second,
     orders:[]
   }
-  while(orders.hasNext()){
-    const order = orders.next();
-    obj.orders.push(order);
-  }
-  db.reports.insertOne(obj);
+  const massive = db.orders.aggregate([{
+    $match:{
+      customersId:customer._id
+    }
+  },
+    {
+      $group:{
+        _id:'$product'
+        ,
+        total:{
+          $sum:'$count'
+        }
+      }
+    }
+  ])
+  obj.orders = massive.toArray();
+  db.reports.insertOne(obj)
 }
-db.reports.aggregate([{$group:{}}])
 
-
-
+function func(size,pages) {
+  for (let i = 0;i <pages;i++){
+    const customers = db.customers.find().limit(size).skip(i * size)
+    print(customers.toArray())
+  }
+}
+func(10,5)
